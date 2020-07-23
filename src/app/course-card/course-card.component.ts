3c87cb15cbe5e11e74b27f0b09c2adf375877ac4
David Millan <hello@davidmillan.me>
2020-07-23 23:59:31 +0200

    play with output

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Course } from '../model/course';
+import { COURSES } from '../../db-data';
 
 @Component({
   selector: 'course-card',
@@ -11,9 +12,18 @@ export class CourseCardComponent implements OnInit {
   @Input()
   course:Course;
 
-  constructor() { }
+  @Output()
+  courseSelected = new EventEmitter<Course>();
+
+  constructor() {}
 
   ngOnInit(): void {
+  }
+
+  onCourseViewed() {
+    console.log('hello');
+
+    this.courseSelected.emit(this.course);
   }
 
 }
