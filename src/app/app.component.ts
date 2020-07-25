11f94e9148051a248fe86342aa09fdf518cb1729
David Millan <hello@davidmillan.me>
2020-07-25 20:53:01 +0200

    Built-In Pipes - Complete Catalog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,16 @@ export class AppComponent {
 
   courses = COURSES;
 
+  course = this.courses[0];
+
+  title = COURSES[0].description;
+
+  price = 9.99;
+  
+  rate = 0.67;
+
+  startDate = new Date(2020, 0, 1);
+
   onCourseSelected(course:Course) {
     console.log('helloCard', course);
   }
